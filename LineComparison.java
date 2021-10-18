2eccde234b5dd3c8557b4b0d197062fc9d620726
[somilpatil] <[patilsomil118@gmail.com]>
2021-10-19 01:12:38 +0530

    Added: LenghtOfLine

--- LineComparison.java
+++ LineComparison.java
@@ -2,6 +2,13 @@ package com.bl;
 
 public class LineComparison {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program");
+		double x1 = 1 ;
+		double x2 = 2 ;
+		double y1 = 3 ;
+		double y2 = 4 ;
+		double Distance;
+   
+		Distance = Math.sqrt(Math.pow(x2 - x1, 2))+(Math.pow(y2-y1,2)); 
+		System.out.println("The lenght of line is :" +Distance);
 	}
 }
